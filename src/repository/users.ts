7a4dabc8e967d2307df8a4bbda85874fd4bafb95
Bray Pulido <brayan.pulido@finaktiva.com>
2024-01-29 10:35:35 -0500

    add ajusted in filed

--- src/repository/users.ts
+++ src/repository/users.ts
@@ -3,9 +3,6 @@ import { connectToSqlServer } from "../DB/config";
 import bcrypt from 'bcrypt';
 
 
-
-
-
 export const createUser = async (data: dataExample): Promise<IresponseRepositoryService> => {
   try {
     const { email, password, tableName } = data;
